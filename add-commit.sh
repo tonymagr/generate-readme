4b77f4801ad18d851fd9fd6c139636d8bb08737b
Tony Magrady <tonymagrady@gmail.com>
2023-11-05 19:59:29 -0800

    challenge 09- demo Sun, Nov  5, 2023  7:59:29 PM

--- add-commit.sh
+++ add-commit.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+echo "Run git add and status"
+echo "-------------"
 git add .
+git status
+echo " "
+echo "Run git commit"
+echo "--------------"
 git commit -m "challenge 09- demo $(date)"
+echo " "
+echo "Run git push"
+echo "------------"
 git push
